df7bc025b46d392401471168cd66f625520f9817
Mikulic <zara.relja1988@gmail.com>
2017-11-30 23:59:43 +0100

    Poboljšano
    Jednostavna desktop aplikacija
    Audio video player

---
+++ Form1.Designer.vb
@@ -0,0 +1,151 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class AVP
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(AVP))
+        Me.Prvi = New System.Windows.Forms.Button()
+        Me.Odaberi = New System.Windows.Forms.OpenFileDialog()
+        Me.Slika = New System.Windows.Forms.PictureBox()
+        Me.Tekst = New System.Windows.Forms.TextBox()
+        Me.WMP = New AxWMPLib.AxWindowsMediaPlayer()
+        Me.Lista = New System.Windows.Forms.ListBox()
+        Me.Drugi = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.IzbrisiSve = New System.Windows.Forms.Button()
+        Me.Lista2 = New System.Windows.Forms.ListBox()
+        CType(Me.Slika, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.WMP, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Prvi
+        '
+        Me.Prvi.Location = New System.Drawing.Point(14, 8)
+        Me.Prvi.Name = "Prvi"
+        Me.Prvi.Size = New System.Drawing.Size(130, 36)
+        Me.Prvi.TabIndex = 1
+        Me.Prvi.Text = "Pretraži mapu"
+        Me.Prvi.UseVisualStyleBackColor = True
+        '
+        'Odaberi
+        '
+        Me.Odaberi.Multiselect = True
+        '
+        'Slika
+        '
+        Me.Slika.BackColor = System.Drawing.Color.Gray
+        Me.Slika.Location = New System.Drawing.Point(-8, 0)
+        Me.Slika.Name = "Slika"
+        Me.Slika.Size = New System.Drawing.Size(891, 49)
+        Me.Slika.TabIndex = 2
+        Me.Slika.TabStop = False
+        '
+        'Tekst
+        '
+        Me.Tekst.Font = New System.Drawing.Font("Lucida Calligraphy", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Tekst.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Tekst.Location = New System.Drawing.Point(290, 1)
+        Me.Tekst.Multiline = True
+        Me.Tekst.Name = "Tekst"
+        Me.Tekst.Size = New System.Drawing.Size(586, 47)
+        Me.Tekst.TabIndex = 3
+        Me.Tekst.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'WMP
+        '
+        Me.WMP.Enabled = True
+        Me.WMP.Location = New System.Drawing.Point(-8, 55)
+        Me.WMP.Name = "WMP"
+        Me.WMP.OcxState = CType(resources.GetObject("WMP.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.WMP.Size = New System.Drawing.Size(629, 406)
+        Me.WMP.TabIndex = 0
+        '
+        'Lista
+        '
+        Me.Lista.FormattingEnabled = True
+        Me.Lista.Location = New System.Drawing.Point(622, 55)
+        Me.Lista.Name = "Lista"
+        Me.Lista.Size = New System.Drawing.Size(261, 329)
+        Me.Lista.TabIndex = 4
+        '
+        'Drugi
+        '
+        Me.Drugi.Location = New System.Drawing.Point(150, 8)
+        Me.Drugi.Name = "Drugi"
+        Me.Drugi.Size = New System.Drawing.Size(132, 36)
+        Me.Drugi.TabIndex = 6
+        Me.Drugi.Text = "Pretraži pjesmu"
+        Me.Drugi.UseVisualStyleBackColor = True
+        '
+        'IzbrisiSve
+        '
+        Me.IzbrisiSve.Location = New System.Drawing.Point(691, 390)
+        Me.IzbrisiSve.Name = "IzbrisiSve"
+        Me.IzbrisiSve.Size = New System.Drawing.Size(110, 41)
+        Me.IzbrisiSve.TabIndex = 10
+        Me.IzbrisiSve.Text = "Izbriši sve"
+        Me.IzbrisiSve.UseVisualStyleBackColor = True
+        '
+        'Lista2
+        '
+        Me.Lista2.FormattingEnabled = True
+        Me.Lista2.Location = New System.Drawing.Point(656, 263)
+        Me.Lista2.Name = "Lista2"
+        Me.Lista2.Size = New System.Drawing.Size(120, 95)
+        Me.Lista2.TabIndex = 11
+        Me.Lista2.Visible = False
+        '
+        'AVP
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(884, 462)
+        Me.Controls.Add(Me.Lista2)
+        Me.Controls.Add(Me.IzbrisiSve)
+        Me.Controls.Add(Me.Drugi)
+        Me.Controls.Add(Me.Lista)
+        Me.Controls.Add(Me.Tekst)
+        Me.Controls.Add(Me.Prvi)
+        Me.Controls.Add(Me.WMP)
+        Me.Controls.Add(Me.Slika)
+        Me.Name = "AVP"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "AVPlayer"
+        CType(Me.Slika, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.WMP, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents WMP As AxWMPLib.AxWindowsMediaPlayer
+    Friend WithEvents Prvi As Button
+    Friend WithEvents Odaberi As OpenFileDialog
+    Friend WithEvents Slika As PictureBox
+    Friend WithEvents Tekst As TextBox
+    Friend WithEvents Lista As ListBox
+    Friend WithEvents Drugi As Button
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents IzbrisiSve As Button
+    Friend WithEvents Lista2 As ListBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,97 @@
+﻿Public Class AVP
+    Private Playlist As WMPLib.IWMPPlaylist
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Tekst.Text = "Dobrodošli!"
+        Dim folderPath As String
+        WMP.settings.volume = 50
+        'AxWindowsMediaPlayer1.uiMode = "none"
+        WMP.settings.setMode("loop", True)
+        Playlist = WMP.newPlaylist("MyPlayList", "")
+        WMP.currentPlaylist = Playlist
+        ' AxWindowsMediaPlayer1.Ctlcontrols.play()
+        ' WMP.Ctlcontrols.play()
+    End Sub
+    Private Sub Prvi_Click(sender As Object, e As EventArgs) Handles Prvi.Click
+        'FolderBrowserDialog1.ShowDialog()
+        WMP.URL = ""
+        If FolderBrowserDialog1.ShowDialog = DialogResult.OK Then
+            'Prvi.Enabled = False
+            'Playlist.clear()
+            For Each video As String In IO.Directory.GetFiles(FolderBrowserDialog1.SelectedPath, "*.mp3")
+                Playlist.appendItem(WMP.newMedia(video.LastIndexOf("\") + 1))
+                '  If (Playlist.Equals(Lista.Items)) Then
+                ' If (Playlist.Exists(Lista.Items)) Then
+                ' Else
+                '    Me.Lista.Items.Add(IO.Path.GetFileNameWithoutExtension(video) + (System.IO.Path.GetFullPath(video)))
+                Lista.Items.Add(IO.Path.GetFileNameWithoutExtension(video))
+                Lista2.Items.Add(video)
+                '            Lista.Items.Add(video.Substring(video.LastIndexOf("\") + 1))
+                'End If
+            Next
+            WMP.Ctlcontrols.play()
+        End If
+        'WMP.Ctlcontrols.play()
+    End Sub
+    Private Sub Drugi_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Drugi.Click
+        Odaberi.ShowDialog()
+        Odaberi.Multiselect = True
+        Dim NazivPjesme As String = ""
+        Odaberi.Filter = "(mp3,wav,mp4,mov,wmv,mpg,avi,3gp,flv)|*.mp3;*.wav;*.mp4;*.3gp;*.avi;*.mov;*.flv;*.wmv;*.mpg|all files|*.*"
+        Dim podjela As Integer
+        Dim tocka As Integer
+        If DialogResult.OK Then
+            WMP.URL = Odaberi.FileName
+            For Each track As String In Odaberi.FileNames
+                'NazivPjesme = track
+                ' If (Lista.Items.Add(track) Then
+                Lista.Items.Add(IO.Path.GetFileNameWithoutExtension(track))
+                Lista2.Items.Add(track)
+                'Lista2.Items.Add(WMP.URL)
+            Next
+        End If
+        Tekst.Text = "Uživajte..."
+    End Sub '
+    Private Sub Lista_SelectedIndexChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles Lista.SelectedIndexChanged
+        Lista2.SelectedIndex = Lista.SelectedIndex
+    End Sub
+    Private Sub Lista_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles Lista.DoubleClick
+        WMP.URL = Lista2.SelectedItem
+    End Sub
+    'Private Sub Izbrisi_Click() Handles Izbriši.Click
+    '    Lista.Items.Remove(Lista.SelectedItem)
+    '    Lista2.Items.Remove(Lista2.SelectedItem)
+    '    Lista.Refresh()
+    '    Lista2.Refresh()
+    'End Sub
+    ' Private Sub Izbrisi_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Izbrisi.Click
+    '    Lista2.Items.RemoveAt(Lista2.SelectedItem)
+    '   Lista.Items.RemoveAt(Lista.SelectedItem)
+    'Lista2.SelectedItem.Clear()
+    'End Sub
+    Private Sub IzbrisiSve_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles IzbrisiSve.Click
+        Lista.Items.Clear()
+        Lista2.Items.Clear()
+        WMP.URL = ""
+    End Sub
+    'Private Sub IzbrisiSve_Click() Handles IzbrisiSve.Click
+    '    Lista.Items.Clear()
+    '    Lista2.Items.Clear()
+    'End Sub
+End Class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
